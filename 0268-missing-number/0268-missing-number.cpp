ed2bd0b2b12d357d352006a5917f64714b238f29
Arsh Bhardwaj <143160236+Arsh2924@users.noreply.github.com>
2024-10-18 16:55:38 +0530

    Time: 0 ms (100%), Space: 20.7 MB (75.83%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,23 +2,12 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        
-        // Initialize a hash vector of size n+1, filled with 0s
-        vector<int> hash(n + 1, 0);
-        
-        // Mark presence of each number in nums by setting hash[nums[i]] to 1
-        for (int i = 0; i < n; i++) {
-            hash[nums[i]] = 1;
-        }
-        
-        // Find the missing number by checking for a 0 in the hash array
-        for (int i = 0; i <= n; i++) {
-            if (hash[i] == 0) {
-                return i; // Missing number
-            }
-        }
-        
-        return -1; // Safeguard, should not reach here
+       int sum = (n*(n+1))/2;
+       int actual = 0;
+       for(int i=0;i<n;i++){
+            actual+=nums[i];
+       }
+       return sum-actual;
     }
 };
 
