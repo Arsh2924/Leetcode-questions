aa636e53c749eccf30bd42819bf53eb7bc9dcd19
Arsh Bhardwaj <143160236+Arsh2924@users.noreply.github.com>
2024-11-16 19:05:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+         int revNum = 0;
+    // Create a duplicate variable to
+    // store the original number
+    int dup = x;
+    // Iterate through each digit of
+    // the number until it becomes 0
+    while (x != 0) {
+        // Extract the last
+        // digit of the number
+        int ld = x % 10;
+        // Build the reverse number
+        // by appending the last digit
+        revNum = (revNum * 10) + ld;
+        // Remove the last digit
+        // from the original number
+        x = x / 10;
+    }
+    // Check if the original number
+    // is equal to its reverse
+    if (dup == revNum) {
+        // If equal, return true
+        // indicating it's a palindrome
+        return true;
+    } else {
+        // If not equal, return false
+        // indicating it's not a palindrome
+        return false;
+    }
+    }
+};
