dc4f3601d57d4a3734be2a3276934e234922b91c
Arsh Bhardwaj <143160236+Arsh2924@users.noreply.github.com>
2025-03-27 10:38:24 +0530

    Time: 0 ms (100%), Space: 9.8 MB (34.23%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-         vector<vector<int>>  generate(numRows);
-   for(int i=0;i<numRows;i++){
-    generate[i].resize(i + 1);
-    generate[i][0] = generate[i][i] = 1;
-    for(int j=0;j<i;j++){
-   
-   
-         generate[i][j]= generate[i-1][j]+ generate[i-1][j-1];
-    
-    }
-   }
-   return  generate;
+        vector<vector<int>> generate(numRows); // Create outer vector
+        
+        for (int i = 0; i < numRows; i++) {
+            generate[i].resize(i + 1); // Resize inner vector
+            
+            generate[i][0] = generate[i][i] = 1; // First and last element of row = 1
+            
+            for (int j = 1; j < i; j++) { // Loop only for valid middle elements
+                generate[i][j] = generate[i-1][j-1] + generate[i-1][j];
+            }
+        }
+        return generate;
     }
 };
