ce288c1dfee288cf1896ba50a057283e886d9842
Arsh Bhardwaj <143160236+Arsh2924@users.noreply.github.com>
2025-07-15 22:28:20 +0530

    Time: 44 ms (96.71%), Space: 8.8 MB (97.41%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,36 +1,33 @@
 class Solution {
 private:
-    // Function to get the minimum non-zero frequency
-    int getmincount(int freq[]) {
-        int mincount = INT_MAX;
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] != 0) {
-                mincount = min(mincount, freq[i]);
-            }
+int getmincount(int freq[]){
+    int mini = INT_MAX;
+    for(int i =0;i<26;i++){
+        if(freq[i]!=0){
+            mini = min(mini,freq[i]);
         }
-        return mincount;
     }
-
-    // Function to get the maximum frequency
-    int getmaxcount(int freq[]) {
-        int maxcount = 0;
-        for (int i = 0; i < 26; i++) {
-            maxcount = max(maxcount, freq[i]);
-        }
-        return maxcount;
+    return mini;
+}
+   
+   int getmaxcount(int freq[]){
+    int maxi = 0;
+    for(int i=0;i<26;i++){
+        maxi = max(maxi,freq[i]);
     }
-
+    return maxi;
+   }
 public:
     int beautySum(string s) {
-        int sum = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int freq[26] = {0}; // Initialize frequency array for each substring
-            for (int j = i; j < s.length(); j++) {
-                freq[s[j] - 'a']++; // Increment frequency of the current character
-                int beauty = getmaxcount(freq) - getmincount(freq);
-                sum += beauty;
-            }
+       int sum = 0;
+       for(int i=0;i<s.length();i++){
+        int freq[26] = {0};
+        for(int j=i;j<s.length();j++){
+            freq[s[j]-'a']++;
+            int beauty = getmaxcount(freq) - getmincount(freq);
+            sum+=beauty;
         }
-        return sum;
+       }
+       return sum;
     }
 };
