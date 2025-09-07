933f52a39de276bbeb8dbf81e90f76d62694490e
Arsh Bhardwaj <143160236+Arsh2924@users.noreply.github.com>
2025-09-07 09:19:15 +0530

    Time: 0 ms (100%), Space: 20.7 MB (58.12%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-    map<long long,int> ans;
-    for(int i=0;i<nums.size();i++){
-       ans[nums[i]]++;     
-     }
-     for(auto i : ans){
-        if(i.second==1){
-            return i.first;
-        }
-     }
-     return -1;
+        int result = 0;
+    for (int num : nums) {
+        result ^= num; // XOR all numbers
+    }
+    return result;
     }
 };
